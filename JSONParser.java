dcc406add353f0cddbc8da2c4588d08dbcfdb2ca
Count D <admin@countd.eu>
2014-02-11 21:03:24 +0200

    Separated the parsing function into more managable chuncks

--- JSONParser.java
+++ JSONParser.java
@@ -3,61 +3,80 @@ import java.util.regex.Pattern;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.Iterator;
-
-// (\"\w+\":\"*[\w\d\.\-\s]*\"*)
-
 public class JSONParser {
+	private static Pattern
+		JSONElementPattern = Pattern.compile("\"(\\w+)\":\"*([\\w\\d\\.\\-\\s]*)\"*");
+
 	private static void printBuses(BusStop[] stops) {
 		for(int i = 0; i < stops.length; i++) {
 			System.out.println(stops[i]);
 		}
 	}
 
-	public static BusStop[] parseNearest(String json) {
-		BusStop[] stops = null;
-		int filledStops = 0;
+	public static BusStop parseNearest(String json, String bus) {
+		return null;
+	}
+
+	private static String getStopsJSON(String json) {
 		Matcher stopsMatcher = Pattern.compile("\"stops\":\\[([\\w\\W]+)\\]").matcher(json);
 		if(stopsMatcher.find()) {
-			String stopsStr = stopsMatcher.group(1);
-			//next, find all stops themselves
-			Matcher stopsInfoMatcher = Pattern.compile("\\{([\\w\"\\-\\d\\s\\.:,])*\\}").matcher(stopsStr);
-			int stopsCount = 0;
-			while(stopsInfoMatcher.find()) {
-				stopsCount++;
-			}
-			stopsInfoMatcher.reset();
-			stops = new BusStop[stopsCount]; // check that it's not zero
-
-			while(stopsInfoMatcher.find()) {
-				String lat = "", lon = "", atco = "";
-				long distance = 0;
-				String infoBlock = stopsInfoMatcher.group();
-				Matcher infoElementMatcher = Pattern.compile("\"(\\w+)\":\"*([\\w\\d\\.\\-\\s]*)\"*").matcher(infoBlock);
-				while(infoElementMatcher.find()) {
-					String elementName = infoElementMatcher.group(1);
-					String elementContent = infoElementMatcher.group(2);
-					if (elementName.equals("atcocode")) {
-						atco = elementContent; // need to strip "s
-					} else if (elementName.equals("latitude")) {
-						lat = elementContent;
-					} else if (elementName.equals("longitude")) {
-						lon = elementContent;
-					} else if (elementName.equals("distance")) {
-						distance = Long.parseLong(elementContent, 10);
-					}
-				}
-				stops[filledStops++] = new BusStop(lat, lon, atco, distance);
-			}
+			return stopsMatcher.group(1);
 		} else {
-			System.out.println("ERROR! 1");
+			return ""; // thow exception here?
+		}
+	}
+
+	private static String[] getStopsInfo(String json) {
+		String[] stops = null;
+		int filledStops = 0;
+		
+		//next, find all stops themselves
+		Matcher stopsInfoMatcher = Pattern.compile("\\{([\\w\"\\-\\d\\s\\.:,])*\\}").matcher(json);
+		int stopsCount = 0;
+		while(stopsInfoMatcher.find()) {
+			stopsCount++;
+		}
+		stopsInfoMatcher.reset();
+		stops = new String[stopsCount]; // check that it's not zero
+
+		while(stopsInfoMatcher.find()) {
+			String infoBlock = stopsInfoMatcher.group();
+			stops[filledStops++] = infoBlock;
 		}
 		
-
 		return stops;
 	}
 
+	public static BusStop[] parseNearestAsArray(String json) {
+		String[] stops = getStopsInfo(getStopsJSON(json));
+		int filledStops = 0;
+		BusStop[] busStops = new BusStop[stops.length];
+		for(String infoBlock : stops) {
+			String lat = "", lon = "", atco = "";
+			long distance = 0;
+			Matcher infoElementMatcher = JSONElementPattern.matcher(infoBlock);
+			while(infoElementMatcher.find()) {
+				String elementName = infoElementMatcher.group(1);
+				String elementContent = infoElementMatcher.group(2);
+				if (elementName.equals("atcocode")) {
+					atco = elementContent;
+				} else if (elementName.equals("latitude")) {
+					lat = elementContent;
+				} else if (elementName.equals("longitude")) {
+					lon = elementContent;
+				} else if (elementName.equals("distance")) {
+					distance = Long.parseLong(elementContent, 10);
+				}
+			}
+			busStops[filledStops++] = new BusStop(lat, lon, atco, distance);
+		}
+		
+		return busStops;
+	}
+	
+
 	public static String[] parseStop(String json) {
-		Matcher infoElementMatcher = Pattern.compile("\"(\\w+)\":\"*([\\w\\d\\.\\-\\s]*)\"*").matcher(json);
+		Matcher infoElementMatcher = JSONElementPattern.matcher(json);
 		Set<String> busSet = new HashSet<String>();
 		while(infoElementMatcher.find()) {
 			String elementName = infoElementMatcher.group(1);
@@ -78,7 +97,7 @@ public class JSONParser {
 	}
 
 	public static void main(String[] args) {
-		BusStop[] stops = parseNearest(DataFetcher.fetchNearestStops("53.794333", "-1.7675"));
+		BusStop[] stops = parseNearestAsArray(DataFetcher.fetchNearestStops("53.794333", "-1.7675"));
 		printBuses(stops);
 
 	}
